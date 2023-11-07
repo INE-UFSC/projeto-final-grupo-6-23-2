f153a51ab526541c10da2ad08a25d6ecbb404eb9
Lucas de Souza Vieira <dev.lucas.sv@gmail.com>
2023-11-07 09:39:08 -0300

    lava mudanças

--- versao_final/entidades/lava.py
+++ versao_final/entidades/lava.py
@@ -2,23 +2,30 @@ import pygame
 from constantes.constantes import Constantes
 
 
-class Lava:
+class Lava(pygame.sprite.Sprite):
     """Classe responsável pela lava"""
 
     def __init__(self):
+        super().__init__()
         self.__constantes = Constantes()
-        self.__superficie = pygame.Surface((self.__constantes.largura_tela, self.__constantes.altura_tela * 0.2))
-        self.__superficie.fill('Orange')
-        self.__posicao = (0, self.__constantes.altura_tela * 0.8)
-        self.__rect = pygame.Rect(*self.__posicao, self.__constantes.largura_tela, self.__constantes.altura_tela * 0.2)
+        self.__image = pygame.image.load("versao_final/styles/assets/sprite_lava_2.png")
+        self.__image = pygame.transform.scale(self.__image, (500, 150))
+        self.__mask_image = pygame.mask.from_surface(self.__image)
+        self.__rect = self.__mask_image.get_rect()
+
+        self.rect.x = 0
+        self.rect.y = self.__constantes.altura_tela * 0.85
+
+    def uptade(self):
+        pass
 
     @property
-    def superficie(self) -> pygame.Surface:
-        return self.__superficie
-
+    def superficie(self) -> pygame.sprite.Sprite:
+        return self.__image
+    
     @property
     def posicao(self) -> tuple:
-        return self.__posicao
+        return (self.__rect.x, self.__rect.y)
     
     @property
     def rect(self):
