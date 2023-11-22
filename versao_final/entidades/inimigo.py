febf5bb9bdf4d0886caec971c07e3fbff8cb3d8b
Jonatan Hartmann <jonatanhartamann@gmail.com>
2023-11-21 22:08:28 -0300

    Iniciando movimento lateral de inimigos fixos

--- versao_final/entidades/inimigo.py
+++ versao_final/entidades/inimigo.py
@@ -2,6 +2,8 @@ import pygame
 import json
 import random
 from os import path
+import math
+from constantes.constantes import Constantes
 
 
 class Inimigo(pygame.surface.Surface):
@@ -10,32 +12,67 @@ class Inimigo(pygame.surface.Surface):
             path.dirname(__file__), "../styles/assets/inimigos/inimigos.json"
         )
         self.__style = self.set_style()
+        print(self.__style['sprite'])
+
+        self.__constantes = Constantes()
 
         self.__image = pygame.image.load(f"{self.__style['sprite']}tile000.png").convert_alpha()
         self.__image = pygame.transform.scale(self.__image, (48, 48))
         self.__mask_image = pygame.mask.from_surface(self.__image)
         self.__rect = self.__mask_image.get_rect()
 
-        self.__rect.x = 100
-        self.__rect.y = 100
+        self.__rect.x = self.__style['posicaoInicial']['x']
+        self.__rect.y = self.__style['posicaoInicial']['y']
 
         self.__id_tile = 0
         self.__ctrl_tick = 0
+
+        self.__velocidade_base_y = self.__constantes.cenario_veloc_base
+        self.__direcao = "direita"
 
     def update(self):
         self.__ctrl_tick += 1
         if self.__ctrl_tick == 8:
             self.__ctrl_tick = 0
             self._animar()
+        self._movimentar()
 
     def set_style(self):
+        random.seed()
         with open(self.__styles, "r") as file:
             data = json.load(file)
             keys = list(data.keys())
             key_random = random.choice(keys)
             style = data[key_random]
 
+        #style = data["Aqua"]
         return style
+
+    def _movimentar(self):
+        if self.__style['gravidade']:
+            if self.__direcao == 'esquerda':
+                self.rect.x -= 0.5
+            elif self.__direcao == 'direita':
+                self.rect.x += 0.5
+
+            if self.rect.x > 401 and self.__direcao == 'direita':
+                self.__direcao =  'esquerda'
+                self.__image = pygame.transform.flip(self.__image, True, False)
+            elif self.rect.x < 48:
+                self.__direcao = "direita"
+                self.__image = pygame.transform.flip(self.__image, True, False)
+
+            print(self.rect.x)
+
+            self.rect.y += self.__velocidade_base_y
+            self.__velocidade_base_y += self.__constantes.aceleracao_cenario
+        else:
+            self.rect.x += 0.5
+             # A*sen(B*x) + C
+            A = self.__style['movimento']['A']
+            B = self.__style['movimento']['B']
+            C = self.__style['movimento']['C']
+            self.rect.y = A*math.sin((B*self.rect.x)) + C
 
     def _animar(self):
         self.__id_tile = 0 if self.__id_tile == 3 else self.__id_tile + 1
